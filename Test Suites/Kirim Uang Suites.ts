b300a24616ca536abedf9ad2c6639f2906682b6d
fauzan <fauzan.afianto@xtremax.com>
2020-09-16 14:41:28 +0700

    Adding Test Suites & Report

---
+++ Test Suites/Kirim Uang Suites.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Kirim Uang Suites</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>64bbac8c-eec6-40ad-b59a-bf1c4bb8a87a</testSuiteGuid>
+   <testCaseLink>
+      <guid>150f44ce-9ffb-406e-ba53-d3f9efb631b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Abort With Valid Input</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>593987b0-7136-4e45-b6a4-fbdfb3348de4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Go to Kirim Uang Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77c28189-9415-43ad-93cb-cd4069f65191</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Proceeed With Blank Input</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eed0e273-101b-4e54-8d7a-08c3137b9dca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Proceeed With Invalid Beneficiary Number</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>966e0660-72d2-4f4e-8196-000769af294a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Proceeed With Valid Input</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>126694cf-32e3-4a56-b2f8-a88023aa5de2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kirim Uang/Save With Valid Input</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
